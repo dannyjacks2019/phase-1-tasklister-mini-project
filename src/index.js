b717134361d0a26ce4f3638dc1d76a0462716f45
Dan Jackson <dannyjacks2019@gmail.com>
2023-05-30 20:11:16 -0400

    mini project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-});
+const newTaskForm = document.getElementById("create-task-form");
+const newTaskDescription = document.getElementById("new-task-description");
+const newTaskPriority = document.getElementById("new-task-priority")
+
+const newTaskUl = document.getElementById("tasks")
+
+newTaskForm.addEventListener("submit", createNewTask)
+})
+const createNewTask = event => {
+  event.preventDefault()
+  const newTaskDescription = docuement.getElementById("new-task-description")
+  const newTask = document.createElement("li")
+  newTask.innerText = newTaskDescription.value
+
+  appendNewtask(newTask)
+  event.target.reset()
+}
